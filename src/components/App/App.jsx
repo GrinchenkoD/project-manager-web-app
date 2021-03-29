74f6805b447086603fc05d153713a86eb59aa2d5
MikeYef96 <mike.yef@gmail.com>
2021-03-29 19:23:12 +0300

    Madal>>>>>Modal

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,12 @@
-
 import React from 'react';
-import Modal from '../Madal/Modal';
+import Modal from '../Modal/Modal';
 
 const App = function () {
-  return (<div>
-            <Modal />
-          </div>);
+  return (
+    <div>
+      <Modal />
+    </div>
+  );
 };
 
 export default App;
