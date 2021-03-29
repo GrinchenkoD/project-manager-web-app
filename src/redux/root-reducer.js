31b1975b0fa7e20af6218a7c20af3e36e4006fc1
MikeYef96 <mike.yef@gmail.com>
2021-03-29 16:39:50 +0300

    root-reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,2 @@
 import { combineReducers } from 'redux';
 import { createReducer } from '@reduxjs/toolkit';
-
-const items = createReducer([], {});
-
-export default combineReducers({
-  items,
-});
