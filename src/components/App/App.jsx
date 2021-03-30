d18f50dad081f60de30b4d5dd1d1d11fc9384b9d
MikeYef96 <mike.yef@gmail.com>
2021-03-30 12:55:24 +0300

    modal fixed

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,31 +1,30 @@
-
-import React,{ useState, useEffect } from 'react';
-  import { useHistory } from 'react-router';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router';
 import Main from '../Main/Main';
 import Register from '../../pages/registrationPage/RegistrationPage';
 import AuthBackground from 'pages/registrationPage/AuthBackground';
-import Modal from '../Madal/Modal';
+import Modal from '../Modal/Modal';
 import Header from 'components/Header/Header';
 
-
 const App = function () {
-//    const isAuth = true;
-//   const history = useHistory();
+  //    const isAuth = true;
+  //   const history = useHistory();
 
-//   useEffect(() => {
-//     if (!isAuth) {
-//       history.push('/registration');
-//     }
-//   }, []);
-  
-  return (<div>
-            <Header/>
-          <Register />
-            <AuthBackground/>
-          <Main />
-            <Modal />
-          </div>);
+  //   useEffect(() => {
+  //     if (!isAuth) {
+  //       history.push('/registration');
+  //     }
+  //   }, []);
 
+  return (
+    <div>
+      <Header />
+      <Register />
+      <AuthBackground />
+      <Main />
+      <Modal />
+    </div>
+  );
 };
 
 export default App;
