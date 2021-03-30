15a24887c8a2ead134a3f3eca44b3fc36433940d
Dmitry Grinchenko <ra1d3r@i.ua>
2021-03-30 12:11:05 +0300

    fixed root reducer, add provide and persistgate to index

--- src/redux/projects/project-reducers.js
+++ src/redux/projects/project-reducers.js
@@ -1,71 +1,64 @@
 import { createReducer } from '@reduxjs/toolkit';
-import { combineReducers } from 'redux';
 import {
-  addProjectRequest,
+  // addProjectRequest,
   addProjectSuccess, //
-  addProjectError,
-  addContributorRequest,
+  // addProjectError,
+  // addContributorRequest,
   addContributorSuccess,
-  addContributorError,
-  getProjectRequest,
+  // addContributorError,
+  // getProjectRequest,
   getProjectSuccess, //
-  getProjectError,
-  deleteProjectRequest,
+  // getProjectError,
+  // deleteProjectRequest,
   deleteProjectSuccess, //
-  deleteProjectError,
-  changeProjectTitleRequest,
+  // deleteProjectError,
+  // changeProjectTitleRequest,
   changeProjectTitleSuccess,
-  changeProjectTitleError,
+  // changeProjectTitleError,
 } from './project-actions.js';
 
 const projectsReducer = createReducer([], {
-  [getProjectRequest]: (_, { payload }) => [...payload],
+  [getProjectSuccess]: (_, { payload }) => [...payload],
   [addProjectSuccess]: (state, { payload }) => [...state, payload],
   [deleteProjectSuccess]: (state, payload) => [
     ...state.filter(contact => contact.id !== payload),
   ],
 });
 
-const loadingReducer = createReducer(false, {
-  [addProjectRequest]: () => true,
-  [addContributorRequest]: () => true,
-  [getProjectRequest]: () => true,
-  [deleteProjectRequest]: () => true,
-  [changeProjectTitleRequest]: () => true,
+// const loadingReducer = createReducer(false, {
+//   [addProjectRequest]: () => true,
+//   [addContributorRequest]: () => true,
+//   [getProjectRequest]: () => true,
+//   [deleteProjectRequest]: () => true,
+//   [changeProjectTitleRequest]: () => true,
 
-  [addProjectSuccess]: () => false,
-  [addProjectError]: () => false,
-  [addContributorSuccess]: () => false,
-  [addContributorError]: () => false,
-  [getProjectSuccess]: () => false,
-  [getProjectError]: () => false,
-  [deleteProjectSuccess]: () => false,
-  [deleteProjectError]: () => false,
-  [changeProjectTitleSuccess]: () => false,
-  [changeProjectTitleError]: () => false,
-});
+//   [addProjectSuccess]: () => false,
+//   [addProjectError]: () => false,
+//   [addContributorSuccess]: () => false,
+//   [addContributorError]: () => false,
+//   [getProjectSuccess]: () => false,
+//   [getProjectError]: () => false,
+//   [deleteProjectSuccess]: () => false,
+//   [deleteProjectError]: () => false,
+//   [changeProjectTitleSuccess]: () => false,
+//   [changeProjectTitleError]: () => false,
+// });
 
-const cleanError = () => null;
-const handleError = (_, action) => action.payload;
+// const cleanError = () => null;
+// const handleError = (_, action) => action.payload;
 
-const errorReducer = createReducer(null, {
-  [addProjectRequest]: cleanError,
-  [addContributorRequest]: cleanError,
-  [getProjectRequest]: cleanError,
-  [deleteProjectRequest]: cleanError,
-  [changeProjectTitleRequest]: cleanError,
+// const errorReducer = createReducer(null, {
+//   [addProjectRequest]: cleanError,
+//   [addContributorRequest]: cleanError,
+//   [getProjectRequest]: cleanError,
+//   [deleteProjectRequest]: cleanError,
+//   [changeProjectTitleRequest]: cleanError,
 
-  [addProjectError]: handleError,
-  [addContributorError]: handleError,
-  [getProjectError]: handleError,
-  [deleteProjectError]: handleError,
-  [changeProjectTitleError]: handleError,
-});
+//   [addProjectError]: handleError,
+//   [addContributorError]: handleError,
+//   [getProjectError]: handleError,
+//   [deleteProjectError]: handleError,
+//   [changeProjectTitleError]: handleError,
+// });
 
-const allProjectsReducer = combineReducers({
-  loading: loadingReducer,
-  error: errorReducer,
-  projects: projectsReducer,
-});
-
-export default allProjectsReducer;
+export default projectsReducer;
