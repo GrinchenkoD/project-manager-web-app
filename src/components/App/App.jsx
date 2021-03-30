bb28d15b4cbf6c45667b422e187e6bea1eead4e1
Roman Skliarenko <vurtnevk@gmail.com>
2021-03-30 09:54:38 +0300

    reg

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+// import RegistrationPage from '../../pages/registrationPage/RegistrationPage'
+import LoginPage from '../../pages/loginPage/LoginPage'
+const App = function () {
+  return (
+    <div >
+{/* <RegistrationPage /> */}
+<LoginPage />
+    </div>
+  );
+};
+
+export default App;
