5ec24ae440bc716be395248a7c3601c62af4bf50
Roman Skliarenko <vurtnevk@gmail.com>
2021-03-30 12:44:47 +0300

    commit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,15 @@
-
+import Register from '../../pages/registrationPage/RegistrationPage'
+import Login from '../../pages/loginPage/LoginPage'
 import React from 'react';
 import Modal from '../Madal/Modal';
+import AuthBackground from 'pages/registrationPage/AuthBackground';
 
 const App = function () {
   return (<div>
-            <Modal />
+            {/* <Modal /> */}
+            {/* <Login /> */}
+            <Register />
+            <AuthBackground/>
           </div>);
 };
 
