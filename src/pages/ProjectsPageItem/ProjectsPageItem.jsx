aafe67fac1879cecb701deb780e26e7955a7b40c
Maxim Kazachuk <dem2bb1@gmail.com>
2021-03-31 14:43:23 +0300

    added delete button projects

--- src/pages/ProjectsPageItem/ProjectsPageItem.jsx
+++ src/pages/ProjectsPageItem/ProjectsPageItem.jsx
@@ -1,54 +1,33 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-// import { deleteProject } from '/redux/projects/projects-operations';
+import { deleteProject } from '../../redux/projects/project-operations';
 
 import sprite from '../../icons/symbol-defs.svg';
 
 import styles from './ProjectsPageItem.module.css';
 
-export default function ProjectsPageItem({
-  id,
-  title,
-  // description,
-  color,
-  children,
-}) {
+export default function ProjectsPageItem({ _id, title, color, children }) {
   const dispatch = useDispatch();
 
   const onDeleteProject = e => {
-    // dispatch(deleteProject());
-    console.log(e);
+    e.currentTarget && dispatch(deleteProject(_id));
   };
 
   return (
     <>
-      {/* <div className={styles.project}> */}
       <div className={styles.projectInfo}>
-        <h4
-          // className={styles.projectTitle} style={ title.length > 10 ? { lineHeight: '18px' } : { lineHeight: '20px' } }
-          className={styles.projectTitle}
-        >
-          {title}
-        </h4>
-        {/* <p className={styles.projectDescription}>{description}</p> */}
+        <h4 className={styles.projectTitle}>{title}</h4>
         <p className={styles.projectDescription}>{children}</p>
-
-        {/* {children} */}
-
         <button
-          data-id={id}
-          onClick={onDeleteProject}
           type="button"
           className={styles.btnDelete}
+          onClick={onDeleteProject}
         >
-          {/* Delete */}
-          {/* <svg className={styles.btnDeleteIcon}> */}
           <svg className={styles.btnDeleteIcon} style={{ fill: color }}>
             <use href={sprite + '#icon-bin'} />
           </svg>
         </button>
       </div>
-      {/* </div> */}
     </>
   );
 }
