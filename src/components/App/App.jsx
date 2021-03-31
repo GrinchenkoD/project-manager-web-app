f9c268506dbad0000d61da05dc2efc78bef24d04
Lougle <louglescom@gmail.com>
2021-03-31 18:33:57 +0300

    btn and modal is done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,33 +2,32 @@ import React, { useState, useEffect } from 'react';
 import { useHistory, useLocation } from 'react-router';
 import Main from '../Main/Main';
 import Header from 'components/Header/Header';
-import Chart from 'components/Chart/Chart';
+// import Chart from 'components/Chart/Chart';
+import ChartModal from 'components/ChartModal/ChartModal';
 // import RegistrationPage from '../../pages/RegistrationPage';
 // import AuthBackground from 'components/AuthBackground/AuthBackground';
 // import Auth from 'pages/authentification/authentification';
-
+import sprite from '../Chart/analytics.svg';
 
 const App = function () {
   const history = useHistory();
-
+  const [isOpenModal, setIsOpenModal] = useState(false);
   useEffect(() => {
     if (history.location.pathname === '/') {
       history.push('/registration');
     }
   }, [history]);
-  
-const styles = {
-  fontFamily: 'sans-serif',
-  textAlign: 'center',
-};
   return (
     <div>
-      {/* <Header /> */}
-      {/* <Main /> */}
-      <div></div>
-      <div style={styles}>
-        < Chart />
-      </div>
+      <Header />
+      <Main />
+      {/* < Chart /> */}
+      <button onClick={() => setIsOpenModal(true)}>
+        <svg>
+          <use href={sprite + '#icon-analytics' }/>
+        </svg>
+      </button>
+      {isOpenModal && <ChartModal onClose={() => setIsOpenModal(false) }/>}
     </div>
   );
 };
--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -2,23 +2,23 @@ import React, {Component} from 'react';
 import { Line } from 'react-chartjs-2';
 
 const data = {
-  labels: ['30 Бер', '31 Бер','01 Квіт','02 Квіт'],
+  labels: ['Планування', '31 Бер','01 Квіт','02 Квіт'],
   datasets: [
     {
-      label: 'My First dataset',
+      label: 'Запланований залишок трудовитрат',
       fill: false,
-      lineTension: 0.1,
+      lineTension: 0.5,
       backgroundColor: 'rgba(75,192,192,0.4)',
-      borderColor: 'rgba(75,192,192,1)',
+      borderColor: 'rgba(255,0,0,1)',
       borderCapStyle: 'butt',
       borderDash: [],
       borderDashOffset: 0.0,
       borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBorderColor: 'rgba(255,0,0,1)',
       pointBackgroundColor: '#fff',
       pointBorderWidth: 1,
       pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBackgroundColor: 'rgba(255,0,0,1)',
       pointHoverBorderColor: 'rgba(220,220,220,1)',
       pointHoverBorderWidth: 2,
       pointRadius: 3,
@@ -26,20 +26,20 @@ const data = {
       data: [65, 59, 80, 81, 56, 55, 40]
     },
     {
-      label: 'My Second dataset',
+      label: 'Актуальний залишок трудовитрат',
       fill: false,
-      lineTension: 0.1,
+      lineTension: 0.5,
       backgroundColor: 'rgba(75,192,192,0.4)',
-      borderColor: 'rgba(150,192,192,1)',
+      borderColor: 'rgba(18,47,170,1)',
       borderCapStyle: 'butt',
       borderDash: [],
       borderDashOffset: 0.0,
       borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBorderColor: 'rgba(18,47,170,1)',
       pointBackgroundColor: '#fff',
       pointBorderWidth: 1,
       pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBackgroundColor: 'rgba(18,47,170,1)',
       pointHoverBorderColor: 'rgba(220,220,220,1)',
       pointHoverBorderWidth: 2,
       pointRadius: 3,
@@ -54,7 +54,7 @@ export default class Chart extends Component {
   render() {
       return (
       <div>
-        <h2>Line Example</h2>
+        <h2>1-st Sprint of 1-st project</h2>
         <Line ref="chart" data={data} />
       </div>
     );
