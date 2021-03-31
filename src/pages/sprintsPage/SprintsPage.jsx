36a6d069971efcafe3bc054a6fa5b6cb3e850ab5
Maxim Kazachuk <dem2bb1@gmail.com>
2021-03-31 13:35:43 +0300

    last update

--- src/pages/sprintsPage/SprintsPage.jsx
+++ src/pages/sprintsPage/SprintsPage.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import sideBarButton from '../../icons/Buttons/sidebarButton.png';
 import styles from './SprintsPage.module.css';
 
 export default function SprintsPage() {
+  const projects = useSelector(state => state.projects);
+
   return (
-    <div className="sprintsContainer">
+    <div className={styles.sprintsContainer}>
       <div className={styles.sprintsSideBar}>
         <div className={styles.sprintsBackContainer}>
           <a href="" className={styles.sprintsBackButton}>
@@ -13,26 +16,13 @@ export default function SprintsPage() {
           </a>
         </div>
         <ul className={styles.sprintsProjectList}>
-          <li className={styles.sprintsProjectItem}>
-            <a className={styles.sprintsBackButton} href="">
-              Проект 1
-            </a>
-          </li>
-          <li className={styles.sprintsProjectItem}>
-            <a className={styles.sprintsBackButton} href="">
-              Проект 2
-            </a>
-          </li>
-          <li className={styles.sprintsProjectItem}>
-            <a className={styles.sprintsBackButton} href="">
-              Проект 3
-            </a>
-          </li>
-          <li className={styles.sprintsProjectItem}>
-            <a className={styles.sprintsBackButton} href="">
-              Проект 4
-            </a>
-          </li>
+          {projects.map(project => (
+            <li className={styles.sprintsProjectItem} key={project._id}>
+              <a className={styles.sprintsBackButton} href="">
+                {project.title}
+              </a>
+            </li>
+          ))}
         </ul>
         <div className={styles.buttonCont}>
           <button className={styles.sprintsButton}>
@@ -41,7 +31,81 @@ export default function SprintsPage() {
           <p className={styles.buttonText}>Создать проект</p>
         </div>
       </div>
-      <div></div>
+      <div className={styles.sprintMainCont}>
+        <div className={styles.sprintUpperCont}>
+          <div className={styles.firstBlockLeft}>
+            <h2 className={styles.mainblockTitle}>Project</h2>
+            <button type="button" className={styles.changeNameButton}></button>
+          </div>
+          <div className={styles.sprintSecondBlock}>
+            <button className={styles.sprintsButton}>
+              <img className="sprintsButtonImg" src={sideBarButton} alt="" />
+            </button>
+            <p className={styles.sprintUpperText}>Создать спринт</p>
+          </div>
+        </div>
+        <div className={styles.addPeopleCont}>
+          <button type="button" className={styles.addButon}>
+            Добавить людей
+          </button>
+        </div>
+        <ul className={styles.sprintsCont}>
+          <li className={styles.sprintsItem}>
+            <p className={styles.sprintTitle}>Sprint Burndown Chart 1</p>
+            <div className={styles.sprintItemText}>
+              <div className={styles.textCont}>
+                <p>Дата начала</p>
+                <p>31 марта</p>
+              </div>
+              <div className={styles.textCont}>
+                <p>Дата оконч.</p>
+                <p>07 апреля</p>
+              </div>
+              <div className={styles.textCont}>
+                <p>Длительность</p>
+                <p>1 час</p>
+              </div>
+            </div>
+            <button type="button" className={styles.deleteButton}></button>
+          </li>
+          <li className={styles.sprintsItem}>
+            <p className={styles.sprintTitle}>Sprint Burndown Chart 1</p>
+            <div className={styles.sprintItemText}>
+              <div className={styles.textCont}>
+                <p>Дата начала</p>
+                <p>31 марта</p>
+              </div>
+              <div className={styles.textCont}>
+                <p>Дата оконч.</p>
+                <p>07 апреля</p>
+              </div>
+              <div className={styles.textCont}>
+                <p>Длительность</p>
+                <p>1 час</p>
+              </div>
+            </div>
+            <button type="button" className={styles.deleteButton}></button>
+          </li>
+          <li className={styles.sprintsItem}>
+            <p className={styles.sprintTitle}>Sprint Burndown Chart 1</p>
+            <div className={styles.sprintItemText}>
+              <div className={styles.textCont}>
+                <p>Дата начала</p>
+                <p>31 марта</p>
+              </div>
+              <div className={styles.textCont}>
+                <p>Дата оконч.</p>
+                <p>07 апреля</p>
+              </div>
+              <div className={styles.textCont}>
+                <p>Длительность</p>
+                <p>1 час</p>
+              </div>
+            </div>
+            <button type="button" className={styles.deleteButton}></button>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
