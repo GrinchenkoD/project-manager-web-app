6b45de9fd309469ecfb12e4752e18a6be3e09033
Maksym <74206337+Maxlemberg@users.noreply.github.com>
2021-04-02 17:33:19 +0300

    datePicker

--- src/components/SprintForm/SprintForm.jsx
+++ src/components/SprintForm/SprintForm.jsx
@@ -6,67 +6,96 @@ import { addSprint } from '../../redux/sprints/sprint-operation';
 import sprintsSchema from './sprintSchema';
 import FormControl from '../formControl/FormControl';
 import styles from '../ProjectForm/ProjectForm.module.css';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+// import './picker.scss';
+
+// import 'react-datepicker/dist/react-datepicker-cssmodules.css';
+import format from 'date-fns/format';
+import { getDay } from 'date-fns';
+import uk from 'date-fns/locale/uk';
 
 const SprintForm = ({ closeModal }) => {
   const dispatch = useDispatch();
   const { projectId } = useParams();
-  const [startDate, setStartDate] = useState(new Date());
+
+  const [startDate, setStartDate] = useState(null);
+
+  const options = {
+    month: 'long',
+    day: 'numeric',
+  };
+
+  const isWeekday = date => {
+    const day = getDay(date);
+    return day !== 0 && day !== 6;
+  };
+
+  const changeData = date => {
+    setStartDate(date);
+  };
 
   const addNeWSprint = useCallback(
     values => {
       console.log(values);
-      dispatch(addSprint(projectId, values));
+      console.log(startDate);
+      const data = { ...values, endDate: format(startDate, 'yyyy-M-d') };
+      dispatch(addSprint(projectId, data));
       closeModal();
     },
-    [dispatch, projectId, closeModal],
+    [dispatch, projectId, closeModal, startDate],
   );
 
   return (
-    <Formik
-      initialValues={{
-        title: '',
-        endDate: `${startDate}`,
-        duration: '',
-      }}
-      validationSchema={sprintsSchema}
-      onSubmit={addNeWSprint}
-    >
-      <Form>
-        <FormControl
-          name="title"
-          type="text"
-          id="title"
-          placeholder="Назва спринта"
-        />
+    <>
+      <Formik
+        initialValues={{
+          title: '',
+          duration: '',
+        }}
+        validationSchema={sprintsSchema}
+        onSubmit={addNeWSprint}
+      >
+        <Form>
+          <FormControl
+            name="title"
+            type="text"
+            id="title"
+            placeholder="Назва спринта"
+          />
+          <FormControl
+            //   label="Password"
+            name="duration"
+            type="text"
+            id="duration"
+            placeholder="Тривалість"
+          />
+          <div className={styles.btnThumb}>
+            <button
+              type="submit"
+              // onSubmit={addNeWSprint}
+              className={styles.acceptBtn}
+            >
+              Готово
+            </button>
+          </div>
+        </Form>
+      </Formik>
 
-        {/* <FormControl
-          label="Password"
-          name="endDate"
-          type="date"
-          id="endDate"
-          placeholder="Дата"
-        /> */}
-        <DatePicker
-          name="endDate"
-          id="endDate"
-          selected={startDate}
-          onChange={date => setStartDate(date)}
-        />
-        <FormControl
-          //   label="Password"
-          name="duration"
-          type="text"
-          id="duration"
-          placeholder="Тривалість"
-        />
-
-        <div className={styles.btnThumb}>
-          <button type="submit" className={styles.acceptBtn}>
-            Готово
-          </button>
-        </div>
-      </Form>
-    </Formik>
+      <DatePicker
+        selected={startDate}
+        onChange={changeData}
+        placeholderText="Дата закінчення"
+        autoComplete="false"
+        dateFormat={
+          startDate ? startDate.toLocaleDateString('ua', options) : ''
+        }
+        filterDate={isWeekday}
+        locale={uk}
+        // calendarClassName="picker"
+        // className="picker"
+      />
+    </>
   );
 };
 
--- src/components/SprintForm/sprintSchema.js
+++ src/components/SprintForm/sprintSchema.js
@@ -2,7 +2,7 @@ import * as Yup from 'yup';
 
 const sprintsSchema = Yup.object().shape({
   title: Yup.string().required('Обов`язкове поле'),
-  endDate: Yup.string().required('Обов`язкове поле'),
+  // endDate: Yup.string().required('Обов`язкове поле'),
   // endDate: Yup.date().default(function () {
   //   return new Date();
   // }),
