749aebb25bb827daf02a4e2bc765c6306644974b
Maxim Kazachuk <dem2bb1@gmail.com>
2021-04-02 13:58:37 +0300

    asdsad

--- src/pages/TasksPage/TasksPage.jsx
+++ src/pages/TasksPage/TasksPage.jsx
@@ -26,7 +26,6 @@ export default function TasksPage() {
 
   const { tasks } = useSelector(getTasks);
 
-
   useEffect(() => {
     dispatch(getTask(sprintId));
   }, [dispatch, sprintId]);
@@ -86,81 +85,86 @@ export default function TasksPage() {
             </div>
           </div>
         </div>
+        <div className={styles.container}>
+          <div className={styles.navigation}>
+            <div className={styles.datePicker}>
+              <div className={styles.navDay}>
+                <button type="button" className={styles.navLeft}>
+                  &lt;
+                </button>
+                <p className={styles.navCurrentDay}> 2 </p>
+                <p className={styles.navTotalDays}> / 12 </p>
+                <button type="button" className={styles.navRight}>
+                  &gt;
+                </button>
+              </div>
+              <p className={styles.navDate}>08.08.2020</p>
+            </div>
 
-        <div className={styles.navigation}>
-          <div className={styles.datePicker}>
-            <div className={styles.navDay}>
-              <button type="button" className={styles.navLeft}>
-                &lt;
-              </button>
-              <p className={styles.navCurrentDay}> 2 </p>
-              <p className={styles.navTotalDays}> / 12 </p>
-              <button type="button" className={styles.navRight}>
-                &gt;
+            <div className={styles.search}>
+              <button type="button" className={styles.searchBBtnMagnify}>
+                <svg className={styles.searchMagnify}>
+                  <use href={sprite + '#magnify-glass'} />
+                </svg>
               </button>
             </div>
-            <p className={styles.navDate}>08.08.2020</p>
           </div>
 
-          <div className={styles.search}>
-            <button type="button" className={styles.searchBBtnMagnify}>
-              <svg className={styles.searchMagnify}>
-                <use href={sprite + '#magnify-glass'} />
-              </svg>
-            </button>
-          </div>
-        </div>
+          <div className={styles.tasks}>
+            <div className={styles.tasksTitle}>
+              <p className={styles.tasksTitleText}> Sprint Burndown Chart 1 </p>
 
-        <div className={styles.tasks}>
-          <div className={styles.tasksTitle}>
-            <p className={styles.tasksTitleText}> Sprint Burndown Chart 1 </p>
+              <button type="button" className={styles.tasksTitleEdit}>
+                <svg className={styles.btnEdit}>
+                  <use href={sprite + '#icon-edit'} />
+                </svg>
+              </button>
+            </div>
 
-            <button type="button" className={styles.tasksTitleEdit}>
-              <svg className={styles.btnEdit}>
-                <use href={sprite + '#icon-edit'} />
-              </svg>
-            </button>
-          </div>
+            <div className={styles.tasksHeader}>
+              <p className={styles.tasksHeaderTitle}>Задача </p>
+              <p className={styles.tasksHeaderPlanned}>Заплановано годин </p>
+              <p className={styles.tasksHeaderUsed}>Витрачено год / день </p>
+              <p className={styles.tasksHeaderTotal}>
+                Витрачено годин (загал.)
+              </p>
+            </div>
 
-          <div className={styles.tasksHeader}>
-            <p className={styles.tasksHeaderTitle}>Задача </p>
-            <p className={styles.tasksHeaderPlanned}>Заплановано годин </p>
-            <p className={styles.tasksHeaderUsed}>Витрачено год / день </p>
-            <p className={styles.tasksHeaderTotal}>Витрачено годин (загал.)</p>
-          </div>
+            {!tasks.length ? (
+              <h2>ljkoiuhiuhiuh</h2>
+            ) : (
+              <ul className={styles.tasksList}>
+                {tasks.map(tasks => (
+                  <TaskPageItem {...tasks} key={tasks._id} />
+                ))}
+              </ul>
+            )}
 
-          {!tasks.length ? (<h2>ljkoiuhiuhiuh</h2>) : (
-          <ul className={styles.tasksList}>
-            {tasks.map(tasks => (
-              <TaskPageItem {...tasks} key={tasks._id} />
-            ))}
-          </ul>
-          )}
-
-          <div className={styles.addTaskSection}>
-            <button
-              type="button"
-              className={styles.btnAdd}
-              onClick={onOpenModal}
+            <div className={styles.addTaskSection}>
+              <button
+                type="button"
+                className={styles.btnAdd}
+                onClick={onOpenModal}
+              >
+                {/* <p className={styles.btnAddIcon}>+</p> */}
+                <img src={addBtn} alt="" className={styles.btnAddIcon} />
+              </button>
+              <p className={styles.addProjectText}>Створити задачу</p>
+            </div>
+            <TemporaryModal
+              onClose={onCloseModal}
+              onOpen={modalOpen}
+              title="Створення задачi"
             >
-              {/* <p className={styles.btnAddIcon}>+</p> */}
-              <img src={addBtn} alt="" className={styles.btnAddIcon} />
-            </button>
-            <p className={styles.addProjectText}>Створити задачу</p>
-          </div>
-          <TemporaryModal
-            onClose={onCloseModal}
-            onOpen={modalOpen}
-            title="Створення задачi"
-          >
-            <TaskForm closeModal={onCloseModal} />
-          </TemporaryModal>
+              <TaskForm closeModal={onCloseModal} />
+            </TemporaryModal>
 
-          <div className={styles.showGraphSection}>
-            <button type="button" className={styles.btnGraph}>
-              <img src={analytics} alt="" className={styles.btnGraphIcon} />
-            </button>
-            {/* <p className={styles.showGraphText}>Створити задачу</p> */}
+            <div className={styles.showGraphSection}>
+              <button type="button" className={styles.btnGraph}>
+                <img src={analytics} alt="" className={styles.btnGraphIcon} />
+              </button>
+              {/* <p className={styles.showGraphText}>Створити задачу</p> */}
+            </div>
           </div>
         </div>
       </div>
