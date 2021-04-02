d3283f3e27f1155149ea14ff7e9aa45d69bf1815
Maxim Kazachuk <dem2bb1@gmail.com>
2021-04-02 09:52:03 +0300

    added reducer

--- src/components/SprintFormPeople/SprintFormPeople.jsx
+++ src/components/SprintFormPeople/SprintFormPeople.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Form, Formik } from 'formik';
 import sprintsSchema from './sprintSchema';
 import FormControl from '../formControl/FormControl';
 import styles from '../ProjectForm/ProjectForm.module.css';
+import { addContributor } from 'redux/projects/project-operations';
+import { useDispatch } from 'react-redux';
+import { useParams } from 'react-router';
 
 const SprintForm = () => {
-  const addPeople = () => {
-    console.log('Add people!!!!');
-  };
+  const dispatch = useDispatch();
+  const { projectId } = useParams();
+
+  const addPeople = useCallback(
+    values => {
+      dispatch(addContributor(projectId, values));
+      console.log(values);
+    },
+    [dispatch, projectId],
+  );
+
   return (
     <Formik
       initialValues={{
